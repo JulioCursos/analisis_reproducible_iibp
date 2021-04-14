43da5c178298f4044648139799e8d2b2d8809798
Julio Torres <juliomystorres@gmail.com>
2021-04-14 17:07:04 -0300

    cree clase 2 con dplyr y algunos lineas

---
+++ clases/mod2_clase2_dplyr.R
@@ -0,0 +1,11 @@
+############-----------MODULO 2: CLASE 2. Manipunaicion de datos usando dplyr----------############
+#=== Gramatica de dplyr
+## Principales verbos
+ select() # seleccionar columnas
+ filter() # seleccionar filas
+ %>% # operador pipe para unir operaciones
+ mutate() # crear nuevas variables
+ summarize(); group_by(); count() # opciones para agrupar
+ join_left(); join_right() # unir tablas basado en una variable en comun
+ arrange() # ordenar una columna
+ rename() # renombrar encabezados de columnas
