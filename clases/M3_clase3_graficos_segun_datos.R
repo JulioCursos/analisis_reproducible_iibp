df6ef44d424ad1d014b31fcd3bb371e6a2a28dd8
Julio Torres <juliomystorres@gmail.com>
2021-05-22 12:38:07 -0300

    renombre archivo

--- clases/M3_clase3_graficos_segun_datos.R
+++ clases/M3_clase3_graficos_segun_datos.R
@@ -1,14 +1,14 @@
 ############-----------MODULO 3: CLASE 3. GRAFICOS SEGUN TIPOS DE DATOS----------############
 library(tidyverse)
 library(ggplot2)
-
+library(plotly)
 # CONTENIDO
 # 3.1. Graficos de barras
-# 3.3. Histogramas
-# 3.4. Graficos de densidad
-
-
-# 3.1. Graficos de barras
+# 3.2. Histogramas
+# 3.3. Graficos de densidad
+# 3.4. Graficos de dispersion
+# 3.5. Boxplots
+#### 3.1. Graficos de barras
 # Problema: necesito plotear variables numericas (y) para diferentes categorias (x)
 # Importante: la altura puede ser el conteo de casos
             # otras veces representa valores
@@ -104,8 +104,8 @@ glimpse(crabs_tb)
 ggplot(crabs_tb, aes(x= sp, y= FL, fill= sex)) + # para agrupar se completa fill con otra variable categorica
   geom_bar(position = "dodge", stat = "identity")
 
+#### 3.2. Histogramas
 
-# 3.2. Histogramas
 # Problema: queres ver la distribucion los datos
 # Solucion 1: histograma
 # variable numerica
@@ -129,7 +129,8 @@ ggplot(iris, aes(x= Petal.Length)) +
   geom_histogram(binwidth = 0.5, color= "black", fill = "white") +
   facet_grid(~ Species)
 
-# 3.4. Graficos de densidad
+
+# 3.3. Graficos de densidad
 # Problema: queres ver la distribucion con la curva de kernel
 # Solucion: graficos de densidad
 # Datos: tipo numerico
@@ -156,7 +157,89 @@ ggplot(iris, aes(x= Sepal.Length)) +
 
 # combinar con histograma
 
-ggplot(iris, aes(x= Sepal.Length, y= ..density..)) +
+ggplot(iris, aes(x= Sepal.Lengthg, y= ..density..)) +
   geom_histogram(bins = 30, color= "black", fill = "white", size= .2) +
   geom_density()
   
+
+#### 3.4. Graficos de dispersion
+# Problema: querer ver la relacion entre 2 variables continuas
+# Solucion: graficos de dispersion
+# cada observacion ser representa por un punto
+
+# plot base
+ggplot(iris, aes(x= Sepal.Length, y= Sepal.Width)) +
+  geom_point()
+# algunas modificacioens
+# tamanio y forma
+ggplot(iris, aes(x= Sepal.Length, y= Sepal.Width)) +
+  geom_point(shape = 19, size= 2 )
+#shapes
+## 1 redondo vacio
+## 2 triangulo
+## 3 cruz
+## 4 x
+## etc
+
+#
+ggplot(iris, aes(x= Sepal.Length, y= Sepal.Width)) +
+  geom_point(aes(color = Species ), size = 3 ) 
+
+ggplot(crabs, aes(x= FL, y = RW)) +
+  geom_point(aes(color = sp, shape= sex), size = 2 )
+
+# cambiar shapes manualmente
+ggplot(crabs, aes(x= FL, y = RW)) +
+  geom_point(aes(color = sp, shape= sex), size = 3 ) +
+  scale_shape_manual(values = c(1, 2))
+
+
+
+
+### 3.5. Boxplots
+# Problema: querer comparar distribuciones
+ggplot(iris, aes(x= Species, y= Sepal.Length)) +
+  geom_boxplot()
+
+?geom_boxplot 
+# estadisticos
+# rango(bigotes): valores maximos y minimos 
+# mediana: divide el 50% de los datos
+# caja: 25 % cuartiles
+
+# modificar outliers
+
+ggplot(iris, aes(x= Species, y= Sepal.Length)) +
+  geom_boxplot(outlier.size =  4,
+               outlier.shape = 19,
+               outlier.color = "red")
+
+# agregar el promedio
+
+ggplot(iris, aes(x = Species, y = Sepal.Length)) +
+  geom_boxplot() +
+  stat_summary(fun = "mean", 
+               geom = "point",
+               shape = 1,
+               size = 2)
+               
+
+
+# 
+ggplot(iris, aes(x= Species, y= Sepal.Length)) +
+  geom_violin()
+
+
+
+
+# animar los graficos
+
+p <- ggplot(iris, aes(x= Species, y= Sepal.Length)) +
+  geom_boxplot()
+
+ggplotly(p)
+
+
+c <- ggplot(crabs, aes(x= FL, y = RW)) +
+  geom_point(aes(color = sp, shape= sex), size = 2 )
+ggplotly(c)
